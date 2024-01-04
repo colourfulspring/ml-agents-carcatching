4b0f26dd0578af0f7bf2c9cc3d1d80f4ffb7b8a9
colourfulspring <ajxcwz@163.com>
2024-01-04 09:30:03 +0800

    add: 12.29t1 test1

---
+++ Project/Assets/ML-Agents/Examples/CarCatching/Scripts/CarAgent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+
+public class CarAgent : Agent
+{
+    private CarCatchingSettings m_CarCatchingSettings;
+    private Rigidbody m_CatchingCarRb; //cached on initialization
+
+    protected override void Awake()
+    {
+        m_CarCatchingSettings = FindObjectOfType<CarCatchingSettings>();
+    }
+
+    public override void Initialize()
+    {
+        // Cache the agent rb
+        m_CatchingCarRb = GetComponent<Rigidbody>();
+    }
+
+    public void FixedUpdate()
+    {
+        // m_CatchingCarRb.AddForce(Vector3.right, ForceMode.VelocityChange);
+        Debug.Log(this.transform.parent.gameObject.name +
+                  ", " + this.name + ": " + this.transform.position + " " +
+                  this.transform.localPosition);
+    }
+}
---
+++ Project/Assets/ML-Agents/Examples/CarCatching/Scripts/CarCatchingEnvController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using UnityEngine;
+
+public class CarCatchingEnvController : MonoBehaviour
+{
+    [System.Serializable]
+    public class CarInfo
+    {
+        public CarAgent Agent;
+        [HideInInspector] public Vector3 StartingPos;
+        [HideInInspector] public Quaternion StartingRot;
+        [HideInInspector] public Vector3 StartingScale;
+        [HideInInspector] public Rigidbody Rb;
+    }
+
+    /// <summary>
+    /// Max Academy steps before this platform resets
+    /// </summary>
+    /// <returns></returns>
+    [Header("Max Environment Steps")] public int MaxEnvironmentSteps = 25000;
+
+    /// <summary>
+    /// The area bounds.
+    /// </summary>
+    [HideInInspector] public Bounds areaBounds;
+
+    /// <summary>
+    /// The ground. The bounds are used to spawn the elements.
+    /// </summary>
+    public GameObject ground;
+
+    public GameObject area;
+
+    //List of Catching Agents On Platform
+    public List<CarInfo> CatchingAgentsList = new List<CarInfo>();
+
+    //List of Running Agents On Platform
+    public List<CarInfo> RunningAgentsList = new List<CarInfo>();
+
+    public bool UseRandomAgentRotation = true;
+    public bool UseRandomAgentPosition = true;
+
+    private CarCatchingSettings m_CarCatchingSettings;
+
+    // private int m_ResetTimer;
+
+    void Start()
+    {
+        // Get the ground's bounds
+        areaBounds = ground.GetComponent<Collider>().bounds;
+        m_CarCatchingSettings = FindObjectOfType<CarCatchingSettings>();
+        foreach (var item in CatchingAgentsList)
+        {
+            item.StartingPos = item.Agent.transform.position;
+            item.StartingRot = item.Agent.transform.rotation;
+            item.StartingScale = item.Agent.transform.localScale;
+            item.Rb = item.Agent.GetComponent<Rigidbody>();
+        }
+
+        foreach (var item in RunningAgentsList)
+        {
+            item.StartingPos = item.Agent.transform.position;
+            item.StartingRot = item.Agent.transform.rotation;
+            item.StartingScale = item.Agent.transform.localScale;
+            item.Rb = item.Agent.GetComponent<Rigidbody>();
+        }
+
+        ResetScene();
+    }
+
+    /// <summary>
+    /// Use the ground's bounds to pick a random spawn position.
+    /// </summary>
+    public Vector3 GetRandomSpawnPos(Quaternion rot)
+    {
+        var foundNewSpawnLocation = false;
+        var randomSpawnPos = Vector3.zero;
+        var fixedY = RunningAgentsList[0].Agent.transform.position.y;
+        var agentHalfExtents = RunningAgentsList[0].StartingScale * 0.5f;
+        while (foundNewSpawnLocation == false)
+        {
+            var randomPosX = Random.Range(-areaBounds.extents.x * m_CarCatchingSettings.spawnAreaMarginMultiplier,
+                areaBounds.extents.x * m_CarCatchingSettings.spawnAreaMarginMultiplier);
+
+            var randomPosZ = Random.Range(-areaBounds.extents.z * m_CarCatchingSettings.spawnAreaMarginMultiplier,
+                areaBounds.extents.z * m_CarCatchingSettings.spawnAreaMarginMultiplier);
+            randomSpawnPos = ground.transform.position + new Vector3(randomPosX, fixedY, randomPosZ);
+            if (Physics.CheckBox(randomSpawnPos, agentHalfExtents, rot) == false)
+            {
+                foundNewSpawnLocation = true;
+            }
+        }
+
+        return randomSpawnPos;
+    }
+
+    Quaternion GetRandomRot()
+    {
+        return Quaternion.Euler(0, Random.Range(0.0f, 360.0f), 0);
+    }
+
+    public void ResetScene()
+    {
+        // m_ResetTimer = 0;
+
+        //Reset Catching Agents
+        foreach (var item in CatchingAgentsList)
+        {
+            var rot = UseRandomAgentRotation ? GetRandomRot() : item.StartingRot;
+            var pos = UseRandomAgentPosition ? GetRandomSpawnPos(rot) : item.StartingPos;
+
+            item.Agent.transform.SetPositionAndRotation(pos, rot);
+            item.Rb.velocity = Vector3.zero;
+            item.Rb.angularVelocity = Vector3.zero;
+        }
+
+        //Reset Running Agents
+        foreach (var item in RunningAgentsList)
+        {
+            var rot = UseRandomAgentRotation ? GetRandomRot() : item.StartingRot;
+            var pos = UseRandomAgentPosition ? GetRandomSpawnPos(rot) : item.StartingPos;
+
+            item.Agent.transform.SetPositionAndRotation(pos, rot);
+            item.Rb.velocity = Vector3.zero;
+            item.Rb.angularVelocity = Vector3.zero;
+        }
+    }
+}
