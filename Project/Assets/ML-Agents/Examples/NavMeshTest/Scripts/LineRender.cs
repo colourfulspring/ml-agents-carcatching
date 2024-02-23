0bc8289d53f9402c482b88bd1de35e260967b0d4
colourfulspring <ajxcwz@163.com>
2024-02-23 17:33:01 +0800

    add: lineRender at NavMeshTest

---
+++ Project/Assets/ML-Agents/Examples/NavMeshTest/Scripts/LineRender.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class LineRender : MonoBehaviour
+{
+    private LineRenderer line;
+    private Vector3 previousPosition;
+    [SerializeField] private float minDistance = 0.1f;
+    [SerializeField, Range(0f, 20f)] private float width = 2f;
+
+    private void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        line.positionCount = 1;
+        previousPosition = transform.position;
+        line.SetPosition(0, previousPosition);
+        line.startWidth = line.endWidth = width;
+    }
+
+    private void Update()
+    {
+        Vector3 currentPosition = transform.position;
+        currentPosition.y = 0f;
+
+        if (Vector3.Distance(currentPosition, previousPosition) > minDistance)
+        {
+            line.positionCount++;
+            line.SetPosition(line.positionCount - 1, currentPosition);
+            previousPosition = currentPosition;
+        }
+    }
+}
